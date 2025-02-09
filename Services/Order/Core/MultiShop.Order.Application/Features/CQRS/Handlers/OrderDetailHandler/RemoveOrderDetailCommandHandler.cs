e83bc74b961a09aebddf8dc697df0736274e4b33
RabiaKandemir <119038074+RabiaKandemir@users.noreply.github.com>
2025-02-09 18:13:07 +0300

    OrderDetail crud.

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/OrderDetailHandler/RemoveOrderDetailCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Commands.AddressCommands;
+using MultiShop.Order.Application.Features.CQRS.Commands.OrderDetailCommands;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandler
+{
+    public class RemoveOrderDetailCommandHandler
+    {
+        private readonly IRepository<OrderDetail> _repository;
+
+        public RemoveOrderDetailCommandHandler(IRepository<OrderDetail> repository)
+        {
+            _repository = repository;
+        }
+        public async Task Handle(RemoveOrderDetailCommand command)
+        {
+            var values = await _repository.GetByIdAsync(command.Id);
+            await _repository.UpdateAsync(values);
+        }
+    }
+}
