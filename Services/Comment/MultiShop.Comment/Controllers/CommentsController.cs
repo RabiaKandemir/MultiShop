6016d6635938aa34d7f037fc7914693b99ed0abd
RabiaKandemir <119038074+RabiaKandemir@users.noreply.github.com>
2025-03-10 02:44:41 +0300

    Comment UI

--- Services/Comment/MultiShop.Comment/Controllers/CommentsController.cs
+++ Services/Comment/MultiShop.Comment/Controllers/CommentsController.cs
@@ -52,5 +52,11 @@ namespace MultiShop.Comment.Controllers
             _context.SaveChanges();
             return Ok("Yorum başarıyla silindi");
         }
+        [HttpGet("CommentListByProductId")]
+        public IActionResult CommentListByProductId(string id)
+        {
+            var value=_context.UserComments.Where(x=>x.ProductId==id).ToList();
+            return Ok(value);
+        }
     }
 }
