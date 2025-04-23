57d3a01f9e8552fbd6083c675769e6c1fd066202
RabiaKandemir <119038074+RabiaKandemir@users.noreply.github.com>
2025-04-24 01:40:10 +0300

    Google Cloud Storage

---
+++ Services/Images/MultiShop.Images.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MultiShop.Images.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
